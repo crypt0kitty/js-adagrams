59e2af5e11d14ce881576bd371deef853f46fb6f
Sandy <sandyvasquez9@gmail.com>
2020-12-11 00:03:37 -0800

    wave 1 complete

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,47 @@
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
-  },
-};
+    const alpha = [
+      "A", "A", "A", "A", "A", "A", "A", "A", "A",
+      "B", "B",
+      "C", "C",
+      "D", "D", "D", "D",
+      "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
+      "F", "F",
+      "G", "G", "G",
+      "H", "H",
+      "I", "I", "I", "I", "I", "I", "I", "I", "I",
+      "J",
+      "K",
+      "L", "L", "L", "L",
+      "M", "M",
+      "N", "N", "N", "N", "N", "N",
+      "O", "O", "O", "O", "O", "O", "O", "O",
+      "P", "P",
+      "Q",
+      "R", "R", "R", "R", "R", "R",
+      "S", "S", "S", "S",
+      "T", "T", "T", "T", "T", "T",
+      "U", "U", "U", "U",
+      "V", "V",
+      "W", "W",
+      "X",
+      "Y", "Y",
+      "Z"
+    ];
+
+    let newAlpha = [];
+
+    for (let i = 0; i < 10; i++) {
+      let idx = Math.floor(Math.random() * alpha.length);
+      newAlpha.push(alpha[idx]);
+      alpha.splice(idx, 1);
+    }
+    return newAlpha;
+  }
+}
+
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
+
